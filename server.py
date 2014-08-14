1b41c3254580eea64a7e5a00caba2c783d852dfc
Alan Du <alanhdu@gmail.com>
2014-08-14 14:12:59 -0400

    Use gevent for threading.

--- server.py
+++ server.py
@@ -1,26 +1,8 @@
-import threading
-
-from flask import Flask, render_template
+import gevent
+from flask import Flask, render_template, copy_current_request_context
 from flask.ext.socketio import SocketIO, emit, join_room
 
 import game
-
-def setInterval(interval):
-    # use closure to set interval
-    def decorator(func):
-        def wrap(*args, **kwargs):
-            stop = threading.Event()
-            def run():
-                while not stop.is_set():
-                    stop.wait(interval)
-                    func(*args, **kwargs)
-
-            t = threading.Timer(0, run)
-            t.daemon = True
-            t.start()
-            return stop
-        return wrap
-    return decorator
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -28,7 +10,15 @@ socketio = SocketIO(app)
 m = game.Game(debug=True)
 
 @app.route("/")
-def index():
+def index(interval=0.2):
+    @copy_current_request_context
+    def run():
+        while True:
+            gevent.sleep(interval)
+            for username, player in m.players.viewitems():
+                socketio.emit("update", str(player), room=username)
+    gevent.spawn(run)   # use separate thread
+
     return render_template("play.html")
 
 @socketio.on("begin")
@@ -50,14 +40,6 @@ def turn(msg):
     user = msg["username"]
     m.players[user].turn(direction)
 
-@setInterval(0.2)       # update once every 0.2 seconds
-def update():
-    print "UPDATING"
-    for username, player in m.players.iteritems():
-        print username
-        emit("update", str(player), room=username)
-
 if __name__ == "__main__":
     app.debug = True
-    update()
     socketio.run(app, port=8080)
