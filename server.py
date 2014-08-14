daf50bcd43b8623a199c9fd5c14e275238452edd
Alan Du <alanhdu@gmail.com>
2014-08-14 13:45:04 -0400

    Move to synchronous platform for bullet firing

--- server.py
+++ server.py
@@ -1,23 +1,31 @@
-import sys
-import traceback
+import threading
 
-from flask import Flask, render_template, url_for
-from flask.ext.socketio import SocketIO, emit
+from flask import Flask, render_template
+from flask.ext.socketio import SocketIO, emit, join_room
 
 import game
+
+def setInterval(interval):
+    # use closure to set interval
+    def decorator(func):
+        def wrap(*args, **kwargs):
+            stop = threading.Event()
+            def run():
+                while not stop.is_set():
+                    stop.wait(interval)
+                    func(*args, **kwargs)
+
+            t = threading.Timer(0, run)
+            t.daemon = True
+            t.start()
+            return stop
+        return wrap
+    return decorator
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 m = game.Game(debug=True)
-
-# TODO make this work!
-def onError(self, t, value, trace):
-    message = "".join(traceback.format_exception(t, value, trace))
-    print "ERROR\t", message
-    emit("error", message)
-
-
 
 @app.route("/")
 def index():
@@ -27,6 +35,7 @@ def index():
 def begin(msg):
     m.addPlayer(msg["username"])
     player = m.players[msg["username"]]
+    join_room(msg["username"])
 
 @socketio.on("move")
 def move(msg):
@@ -39,15 +48,16 @@ def move(msg):
 def turn(msg):
     direction = msg["direction"]
     user = msg["username"]
-
     m.players[user].turn(direction)
 
-@socketio.on("get update")
-def updateMap(msg):
-    user = msg["username"]
-    emit("update", str(m.players[user]))
+@setInterval(0.2)       # update once every 0.2 seconds
+def update():
+    print "UPDATING"
+    for username, player in m.players.iteritems():
+        print username
+        emit("update", str(player), room=username)
 
 if __name__ == "__main__":
     app.debug = True
-    sys.excepthook = onError
+    update()
     socketio.run(app, port=8080)
--- static/events.coffee
+++ static/events.coffee
@@ -4,9 +4,6 @@ socket.on("update", render)
 
 socket.on("error", (msg) -> alert(msg))
 
-requestUpdate = () ->
-    socket.emit("get update", {username:window.username})
-
 $( "#play" ).on "click", (() ->
     window.username = $( "#username" ).val()
     socket.emit('begin', {width:51, height:23, username:username})
@@ -21,20 +18,24 @@ $( window ).keydown ((evt) ->
     key = evt.which
     chr = String.fromCharCode key
 
-    if evt.shiftKey
-        type = "turn"
-    else
-        type = "move"
 
     switch chr
         when 'J' then direction = 'v'
         when 'K' then direction = '^'
         when 'L' then direction = '>'
         when 'H' then direction = '<'
+        when 'F' then fire=True
         else return true
 
     evt.preventDefault()
+    username = window.username
+
     if direction isnt undefined
-        username = window.username
+        if evt.shiftKey
+            type = "turn"
+        else
+            type = "move"
         socket.emit(type, {direction: direction, username: username})
+    else if fire
+        socket.emit(fire, {username: username})
 )
